47c30699cb57d3c05d43b4292a40bf3307167ba2
Zhu Yuxuan <e0424709@u.nus.edu>
2020-09-26 12:07:29 +0800

    Rename Person and AddressBook for files in Storage, Ui etc

--- src/main/java/seedu/address/ui/AttractionCard.java
+++ src/main/java/seedu/address/ui/AttractionCard.java
@@ -7,14 +7,14 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import seedu.address.model.person.Person;
+import seedu.address.model.attraction.Attraction;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * An UI component that displays information of a {@code Attraction}.
  */
-public class PersonCard extends UiPart<Region> {
+public class AttractionCard extends UiPart<Region> {
 
-    private static final String FXML = "PersonListCard.fxml";
+    private static final String FXML = "AttractionListCard.fxml";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -24,7 +24,7 @@ public class PersonCard extends UiPart<Region> {
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
 
-    public final Person person;
+    public final Attraction attraction;
 
     @FXML
     private HBox cardPane;
@@ -42,17 +42,17 @@ public class PersonCard extends UiPart<Region> {
     private FlowPane tags;
 
     /**
-     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     * Creates a {@code AttractionCode} with the given {@code Attraction} and index to display.
      */
-    public PersonCard(Person person, int displayedIndex) {
+    public AttractionCard(Attraction attraction, int displayedIndex) {
         super(FXML);
-        this.person = person;
+        this.address = attraction;
         id.setText(displayedIndex + ". ");
-        name.setText(person.getName().fullName);
-        phone.setText(person.getPhone().value);
-        address.setText(person.getAddress().value);
-        email.setText(person.getEmail().value);
-        person.getTags().stream()
+        name.setText(attraction.getName().fullName);
+        phone.setText(attraction.getPhone().value);
+        address.setText(attraction.getAddress().value);
+        email.setText(attraction.getEmail().value);
+        attraction.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
@@ -65,13 +65,13 @@ public class PersonCard extends UiPart<Region> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof PersonCard)) {
+        if (!(other instanceof AttractionCard)) {
             return false;
         }
 
         // state check
-        PersonCard card = (PersonCard) other;
+        AttractionCard card = (AttractionCard) other;
         return id.getText().equals(card.id.getText())
-                && person.equals(card.person);
+                && attraction.equals(card.attraction);
     }
 }
--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -2,7 +2,7 @@ package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalAttractions.getTypicalTrackPad;
 
 import java.nio.file.Path;
 
@@ -11,8 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.model.AddressBook;
-import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.TrackPad;
+import seedu.address.model.ReadOnlyTrackPad;
 import seedu.address.model.UserPrefs;
 
 public class StorageManagerTest {
@@ -24,7 +24,7 @@ public class StorageManagerTest {
 
     @BeforeEach
     public void setUp() {
-        JsonAddressBookStorage addressBookStorage = new JsonAddressBookStorage(getTempFilePath("ab"));
+        JsonTrackPadStorage addressBookStorage = new JsonTrackPadStorage(getTempFilePath("ab"));
         JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
         storageManager = new StorageManager(addressBookStorage, userPrefsStorage);
     }
@@ -48,21 +48,21 @@ public class StorageManagerTest {
     }
 
     @Test
-    public void addressBookReadSave() throws Exception {
+    public void trackPadReadSave() throws Exception {
         /*
          * Note: This is an integration test that verifies the StorageManager is properly wired to the
-         * {@link JsonAddressBookStorage} class.
-         * More extensive testing of UserPref saving/reading is done in {@link JsonAddressBookStorageTest} class.
+         * {@link JsonTrackPadStorage} class.
+         * More extensive testing of UserPref saving/reading is done in {@link JsonTrackPadStorageTest} class.
          */
-        AddressBook original = getTypicalAddressBook();
-        storageManager.saveAddressBook(original);
-        ReadOnlyAddressBook retrieved = storageManager.readAddressBook().get();
-        assertEquals(original, new AddressBook(retrieved));
+        TrackPad original = getTypicalTrackPad();
+        storageManager.saveTrackPad(original);
+        ReadOnlyTrackPad retrieved = storageManager.readTrackPad().get();
+        assertEquals(original, new TrackPad(retrieved));
     }
 
     @Test
-    public void getAddressBookFilePath() {
-        assertNotNull(storageManager.getAddressBookFilePath());
+    public void getTrackPadFilePath() {
+        assertNotNull(storageManager.getTrackPadFilePath());
     }
 
 }
