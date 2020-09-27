7edabf91e961184084d9157469c309d0bb1440f5
leeyorktat <leeyorktat@gmail.com>
2020-09-27 14:09:29 +0800

    Edit checkstyle

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,8 +2,8 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.model.TrackPad;
 import seedu.address.model.Model;
+import seedu.address.model.TrackPad;
 
 /**
  * Clears the address book.
