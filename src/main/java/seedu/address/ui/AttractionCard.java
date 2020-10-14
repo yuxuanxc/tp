842a184567431a60bb63f69cab2c53ada3e3f4b2
Zhu Yuxuan <e0424709@u.nus.edu>
2020-10-14 21:00:35 +0800

    Edit AttractionCard.java

--- src/main/java/seedu/address/ui/AttractionCard.java
+++ src/main/java/seedu/address/ui/AttractionCard.java
@@ -63,22 +63,26 @@ public class AttractionCard extends UiPart<Region> {
         locale.setText(attraction.getLocation().value);
         description.setText(attraction.getDescription().value);
         openingHours.setText("Opening Hours: " + attraction.getOpeningHours().value);
-        Label priceRange = new Label(attraction.getPriceRange().toString());
-        priceRange.setStyle("-fx-background-color: #800;");
-        Label rating = new Label(attraction.getRating().toString());
-        rating.setStyle("-fx-background-color: #080;");
-
-        Label visited = new Label(attraction.getVisited().toString());
-        if (attraction.getVisited().equals(new Visited("TRUE"))) {
-            visited.setText(attraction.getVisited().toString());
-        } else {
-            visited.setText("Not Visited");
+        if (attraction.getPriceRange().toString() != "") {
+            Label priceRange = new Label(attraction.getPriceRange().toString());
+            priceRange.setStyle("-fx-background-color: #800;");
+            tags.getChildren().add(priceRange);
         }
-        visited.setStyle("-fx-background-color: #9933ff;");
-        tags.getChildren().add(priceRange);
-        tags.getChildren().add(rating);
-        tags.getChildren().add(visited);
-
+        if (attraction.getRating().toString() != "") {
+            Label rating = new Label(attraction.getRating().toString());
+            rating.setStyle("-fx-background-color: #080;");
+            tags.getChildren().add(rating);
+        }
+        if (attraction.getVisited().toString() != "") {
+            Label visited = new Label(attraction.getVisited().toString());
+            if (attraction.getVisited().equals(new Visited("TRUE"))) {
+                visited.setText(attraction.getVisited().toString());
+            } else {
+                visited.setText("Not Visited");
+            }
+            visited.setStyle("-fx-background-color: #9933ff;");
+            tags.getChildren().add(visited);
+        }
         attraction.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
