a351513d906c7e9f366fbfb69ff7e12c4df6bed4
limkoonkiat <lim.koonkiat42@gmail.com>
2020-10-23 18:13:28 +0800

    Move attraction related tests into the relevant folders

--- src/test/java/seedu/address/logic/commands/attraction/ListAttractionCommandTest.java
+++ src/test/java/seedu/address/logic/commands/attraction/ListAttractionCommandTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.commands;
+package seedu.address.logic.commands.attraction;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showAttractionAtIndex;
