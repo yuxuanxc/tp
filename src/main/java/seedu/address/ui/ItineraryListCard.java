aa5423bf406723d3685da6339dd18092531f4303
Robinho98 <ho.robin@yahoo.com.sg>
2020-10-26 16:48:38 +0800

    Integrate UI for itinerary

--- src/main/java/seedu/address/ui/ItineraryListCard.java
+++ src/main/java/seedu/address/ui/ItineraryListCard.java
@@ -1,4 +1,75 @@
 package seedu.address.ui;
 
-public class ItineraryListCard {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.itinerary.Itinerary;
+
+
+
+public class ItineraryListCard extends UiPart<Region>{
+
+    private static final String FXML = "ItineraryListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Itinerary itinerary;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label locale;
+    @FXML
+    private Label startDate;
+    @FXML
+    private Label endDate;
+    @FXML
+    private Label budget;
+    @FXML
+    private Label description;
+
+    /**
+     * Creates a {@code ItineraryCode} with the given {@code Itinerary} and index to display.
+     */
+    public ItineraryListCard(Itinerary itinerary, int displayedIndex) {
+        super(FXML);
+        this.itinerary = itinerary;
+        id.setText(displayedIndex + ". ");
+        name.setText(itinerary.getName().fullName);
+        startDate.setText(itinerary.getStartDate().value);
+        endDate.setText(itinerary.getEndDate().value);
+        description.setText(itinerary.getDescription().value);
+        budget.setText(itinerary.getBudget().value);
+        locale.setText(itinerary.getLocations());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ItineraryListCard)) {
+            return false;
+        }
+
+        // state check
+        ItineraryListCard card = (ItineraryListCard) other;
+        return id.getText().equals(card.id.getText())
+                && itinerary.equals(card.itinerary);
+    }
 }
+
--- src/main/java/seedu/address/ui/ItineraryListPanel.java
+++ src/main/java/seedu/address/ui/ItineraryListPanel.java
@@ -1,4 +1,49 @@
 package seedu.address.ui;
 
-public class ItineraryListPanel {
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.itinerary.Itinerary;
+
+import java.util.logging.Logger;
+
+/**
+ * Panel containing the list of itineraries.
+ */
+public class ItineraryListPanel extends UiPart<Region> {
+    private static final String FXML = "ItineraryListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(ItineraryListPanel.class);
+
+    @FXML
+    private ListView<Itinerary> itineraryListView;
+
+    /**
+     * Creates a {@code ItineraryListPanel} with the given {@code ObservableList}.
+     */
+    public ItineraryListPanel(ObservableList<Itinerary> itineraryList) {
+        super(FXML);
+        itineraryListView.setItems(itineraryList);
+        itineraryListView.setCellFactory(listView -> new ItineraryListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Itinerary} using a {@code ItineraryCard}.
+     */
+    class ItineraryListViewCell extends ListCell<Itinerary> {
+        @Override
+        protected void updateItem(Itinerary itinerary, boolean empty) {
+            super.updateItem(itinerary, empty);
+
+            if (empty || itineraryListView == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new ItineraryListCard(itinerary, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
 }
